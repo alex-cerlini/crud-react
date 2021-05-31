695987393bba96df44f1ec7efe9e744a8ea05a20
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-05-31 13:10:00 -0300

    create firebase.js

---
+++ src/database/firebase.js
@@ -0,0 +1,10 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyA8-udisMx4R7kqk3EXCbl0yB0CSaTgRvI",
+    authDomain: "cadastro-clientes-3964c.firebaseapp.com",
+    projectId: "cadastro-clientes-3964c",
+    storageBucket: "cadastro-clientes-3964c.appspot.com",
+    messagingSenderId: "841171150451",
+    appId: "1:841171150451:web:922b23e5661b3cb2a76c06"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
