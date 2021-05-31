1a15cfa15b41ebdabb2a1c94a03f28d9607c94c7
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-05-31 13:36:40 -0300

    add new row to FormRegister and List clients

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import FormRegister from './FormRegister'
 
 const Register = () => {
   return (
@@ -7,6 +8,15 @@ const Register = () => {
         <div className="container">
           <h1 className="display-4">Cadastro de Clientes</h1>
           <p className="lead">Cadastre seus clientes para melhor organização.</p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-5">
+          <FormRegister/>
+        </div>
+        <div>
+          Lista de clientes que serão listados em breve
         </div>
       </div>
     </div>
