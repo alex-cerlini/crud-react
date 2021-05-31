c31e88746acce2ac695fb20e419187b18e7269e3
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-05-31 13:39:32 -0300

    create initialFieldsValues

--- src/components/FormRegister.js
+++ src/components/FormRegister.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const FormRegister = () => {
+
+  const initialFieldsValues = {
+    fullName: '',
+    phoneNumber: '',
+    email: '',
+    birthDate: ''
+  }
+
   return (
     <h1>FormRegister</h1>
   )
