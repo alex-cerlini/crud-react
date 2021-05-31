2e19d9b81fde3128c32c2a3248c1c7ce77fc0114
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-05-31 13:16:28 -0300

    add import and export to firebase

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,4 +1,6 @@
-  var firebaseConfig = {
+  import firebase from 'firebase'
+
+  const firebaseConfig = {
     apiKey: "AIzaSyA8-udisMx4R7kqk3EXCbl0yB0CSaTgRvI",
     authDomain: "cadastro-clientes-3964c.firebaseapp.com",
     projectId: "cadastro-clientes-3964c",
@@ -6,5 +8,7 @@
     messagingSenderId: "841171150451",
     appId: "1:841171150451:web:922b23e5661b3cb2a76c06"
   };
-  
-  firebase.initializeApp(firebaseConfig);
+
+  let fireDb = firebase.initializeApp(firebaseConfig);
+
+  export default fireDb.database().ref()
