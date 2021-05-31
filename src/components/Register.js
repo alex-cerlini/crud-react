ad70cdc003ff49ebe6cdd12c01d016dc1d3b4397
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-05-31 13:32:05 -0300

    fix class to className in jumbotron

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
-      <div class="jumbotron jumbotron-fluid text-center">
-        <div class="container">
-          <h1 class="display-4">Cadastro de Clientes</h1>
-          <p class="lead">Cadastre seus clientes para melhor organização.</p>
+      <div className="jumbotron jumbotron-fluid text-center">
+        <div className="container">
+          <h1 className="display-4">Cadastro de Clientes</h1>
+          <p className="lead">Cadastre seus clientes para melhor organização.</p>
         </div>
       </div>
     </div>
